7db8164e36da56f6744b4e5f41573668a1c1f2fe
lcferrum <lcferrum@yandex.com>
2016-09-08 03:25:37 +0300

    Better registry and version query

--- vt.cpp
+++ vt.cpp
@@ -16,6 +16,11 @@ extern pIsWow64Process fnIsWow64Process;
 extern pGetProductInfo fnGetProductInfo;
 extern pwine_get_version fnwine_get_version;
 
+typedef struct _LANGANDCODEPAGE {
+	WORD wLanguage;
+	WORD wCodePage;
+} LANGANDCODEPAGE;
+
 //Defines not found in MinGW's windows.h/winnt.h:
 #define SM_WEPOS			0x2003
 #define SM_FUNDAMENTALS		0x2004
@@ -24,6 +29,7 @@ extern pwine_get_version fnwine_get_version;
 //If numerical value is needed, instead of static cast to ULONG_PTR unary addition operator can be used to force printing numerical value
 //Also, these defines only work correctly with specific std::ios::fmtflags and std::ios::fill (see below in main)
 #define COUT_DEC(dec_int)				+dec_int
+#define COUT_FIX(dec_int, dec_width)	std::setw(dec_width)<<+dec_int
 #define COUT_HEX(hex_int, hex_width)	"0x"<<std::hex<<std::setw(hex_width)<<+hex_int<<std::dec
 #define COUT_BOOL(bool_val)				(bool_val?"TRUE":"FALSE")
 
@@ -33,9 +39,10 @@ LabeledValues ProductTypes(LABELED_VALUES_ARG(VER_NT_WORKSTATION, VER_NT_DOMAIN_
 LabeledValues SystemMetrics(LABELED_VALUES_ARG(SM_FUNDAMENTALS, SM_WEPOS, SM_DEBUG, SM_PENWINDOWS, SM_DBCSENABLED, SM_IMMENABLED, SM_SLOWMACHINE, SM_TABLETPC, SM_MEDIACENTER, SM_STARTER, SM_SERVERR2));
 LabeledValues ProcessorArchitectures(LABELED_VALUES_ARG(PROCESSOR_ARCHITECTURE_INTEL, PROCESSOR_ARCHITECTURE_MIPS, PROCESSOR_ARCHITECTURE_ALPHA, PROCESSOR_ARCHITECTURE_PPC, PROCESSOR_ARCHITECTURE_SHX, PROCESSOR_ARCHITECTURE_ARM, PROCESSOR_ARCHITECTURE_IA64, PROCESSOR_ARCHITECTURE_ALPHA64, PROCESSOR_ARCHITECTURE_MSIL, PROCESSOR_ARCHITECTURE_AMD64, PROCESSOR_ARCHITECTURE_IA32_ON_WIN64, PROCESSOR_ARCHITECTURE_NEUTRAL, PROCESSOR_ARCHITECTURE_UNKNOWN));
 LabeledValues ProductInfoTypes(LABELED_VALUES_ARG(PRODUCT_UNDEFINED, PRODUCT_ULTIMATE, PRODUCT_HOME_BASIC, PRODUCT_HOME_PREMIUM, PRODUCT_ENTERPRISE, PRODUCT_HOME_BASIC_N, PRODUCT_BUSINESS, PRODUCT_STANDARD_SERVER, PRODUCT_DATACENTER_SERVER, PRODUCT_SMALLBUSINESS_SERVER, PRODUCT_ENTERPRISE_SERVER, PRODUCT_STARTER, PRODUCT_DATACENTER_SERVER_CORE, PRODUCT_STANDARD_SERVER_CORE, PRODUCT_ENTERPRISE_SERVER_CORE, PRODUCT_ENTERPRISE_SERVER_IA64, PRODUCT_BUSINESS_N, PRODUCT_WEB_SERVER, PRODUCT_CLUSTER_SERVER, PRODUCT_HOME_SERVER, PRODUCT_STORAGE_EXPRESS_SERVER, PRODUCT_STORAGE_STANDARD_SERVER, PRODUCT_STORAGE_WORKGROUP_SERVER, PRODUCT_STORAGE_ENTERPRISE_SERVER, PRODUCT_SERVER_FOR_SMALLBUSINESS, PRODUCT_SMALLBUSINESS_SERVER_PREMIUM, PRODUCT_UNLICENSED, PRODUCT_HOME_PREMIUM_N, PRODUCT_ENTERPRISE_N, PRODUCT_ULTIMATE_N, PRODUCT_WEB_SERVER_CORE, PRODUCT_MEDIUMBUSINESS_SERVER_MANAGEMENT, PRODUCT_MEDIUMBUSINESS_SERVER_SECURITY, PRODUCT_MEDIUMBUSINESS_SERVER_MESSAGING, PRODUCT_SERVER_FOUNDATION, PRODUCT_HOME_PREMIUM_SERVER, PRODUCT_SERVER_FOR_SMALLBUSINESS_V, PRODUCT_STANDARD_SERVER_V, PRODUCT_DATACENTER_SERVER_V, PRODUCT_ENTERPRISE_SERVER_V, PRODUCT_DATACENTER_SERVER_CORE_V, PRODUCT_STANDARD_SERVER_CORE_V, PRODUCT_ENTERPRISE_SERVER_CORE_V, PRODUCT_HYPERV, PRODUCT_STORAGE_EXPRESS_SERVER_CORE, PRODUCT_STORAGE_STANDARD_SERVER_CORE, PRODUCT_STORAGE_WORKGROUP_SERVER_CORE, PRODUCT_STORAGE_ENTERPRISE_SERVER_CORE, PRODUCT_STARTER_N, PRODUCT_PROFESSIONAL, PRODUCT_PROFESSIONAL_N, PRODUCT_SB_SOLUTION_SERVER, PRODUCT_SERVER_FOR_SB_SOLUTIONS, PRODUCT_STANDARD_SERVER_SOLUTIONS, PRODUCT_STANDARD_SERVER_SOLUTIONS_CORE, PRODUCT_SB_SOLUTION_SERVER_EM, PRODUCT_SERVER_FOR_SB_SOLUTIONS_EM, PRODUCT_SOLUTION_EMBEDDEDSERVER, PRODUCT_SOLUTION_EMBEDDEDSERVER_CORE, PRODUCT_SMALLBUSINESS_SERVER_PREMIUM_CORE, PRODUCT_ESSENTIALBUSINESS_SERVER_MGMT, PRODUCT_ESSENTIALBUSINESS_SERVER_ADDL, PRODUCT_ESSENTIALBUSINESS_SERVER_MGMTSVC, PRODUCT_ESSENTIALBUSINESS_SERVER_ADDLSVC, PRODUCT_CLUSTER_SERVER_V, PRODUCT_EMBEDDED, PRODUCT_STARTER_E, PRODUCT_HOME_BASIC_E, PRODUCT_HOME_PREMIUM_E, PRODUCT_PROFESSIONAL_E, PRODUCT_ENTERPRISE_E, PRODUCT_ULTIMATE_E));
+BasicLabeledValues<HKEY> RegistryHives(LABELED_VALUES_ARG(HKEY_CLASSES_ROOT, HKEY_CURRENT_CONFIG, HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE, HKEY_PERFORMANCE_DATA, HKEY_PERFORMANCE_TEXT, HKEY_USERS));
 
 bool PrintRegistryKey(HKEY hive, const char* keypath, const char* value);
-bool PrintFileVersion(const char* fpath);
+bool PrintFileInformation(const char* fpath);
 bool GetVersionWrapper(OSVERSIONINFOEX &osvi_ex);
 
 int main(int argc, char* argv[])
@@ -43,7 +50,7 @@ int main(int argc, char* argv[])
 	Externs::MakeInstance();
 	
 	//This sets fill character to '0' for all subsequent cout outputs 
-	//It only matters when setw in non-zero (and it is reset to zero after every output) so it won't corrupt ordinary integer output
+	//It only matters when setw in non-zero and it is reset to zero after every output
 	std::cout.fill('0');
 
 	//This prevents base from showing in hex/oct output and hex/oct output becomes uppercase
@@ -54,38 +61,38 @@ int main(int argc, char* argv[])
 	
 	OSVERSIONINFOEX osvi_ex;
 	if (GetVersionWrapper(osvi_ex)) {
-		std::cout<<"dwPlatformId = "<<COUT_HEX(osvi_ex.dwPlatformId, 8)<<std::endl;
+		std::cout<<"\tdwMajorVersion = "<<COUT_DEC(osvi_ex.dwMajorVersion)<<std::endl;
+		std::cout<<"\tdwMinorVersion = "<<COUT_DEC(osvi_ex.dwMinorVersion)<<std::endl;
+		std::cout<<"\tdwBuildNumber = "<<COUT_DEC(osvi_ex.dwBuildNumber)<<" = "<<COUT_DEC(HIBYTE(HIWORD(osvi_ex.dwBuildNumber)))<<"."<<COUT_DEC(LOBYTE(HIWORD(osvi_ex.dwBuildNumber)))<<"."<<COUT_DEC((DWORD)LOWORD(osvi_ex.dwBuildNumber))<<std::endl;
+		std::cout<<"\tdwPlatformId = "<<COUT_HEX(osvi_ex.dwPlatformId, 8)<<std::endl;
 		PlatfromIds.Enums(osvi_ex.dwPlatformId, [](const std::string& label, DWORD value, bool unknown, size_t idx){
 			if (unknown)
-				std::cout<<"\tUNKNOWN VALUE ("<<COUT_HEX(value, 8)<<")"<<std::endl;
+				std::cout<<"\t\tUNKNOWN VALUE ("<<COUT_HEX(value, 8)<<")"<<std::endl;
 			else
-				std::cout<<"\t"<<label<<std::endl;
+				std::cout<<"\t\t"<<label<<std::endl;
 			return false;
 		});
-		std::cout<<"dwMajorVersion = "<<COUT_DEC(osvi_ex.dwMajorVersion)<<std::endl;
-		std::cout<<"dwMinorVersion = "<<COUT_DEC(osvi_ex.dwMinorVersion)<<std::endl;
-		std::cout<<"dwBuildNumber = "<<COUT_DEC(osvi_ex.dwBuildNumber)<<" = "<<COUT_DEC(HIBYTE(HIWORD(osvi_ex.dwBuildNumber)))<<"."<<COUT_DEC(LOBYTE(HIWORD(osvi_ex.dwBuildNumber)))<<"."<<COUT_DEC((DWORD)LOWORD(osvi_ex.dwBuildNumber))<<std::endl;
-		std::cout<<"szCSDVersion = \""<<COUT_DEC(osvi_ex.szCSDVersion)<<"\""<<std::endl;
+		std::cout<<"\tszCSDVersion = \""<<COUT_DEC(osvi_ex.szCSDVersion)<<"\""<<std::endl;
 		if (osvi_ex.dwOSVersionInfoSize==sizeof(OSVERSIONINFOEX)) {
-			std::cout<<"wServicePackMajor = "<<COUT_DEC(osvi_ex.wServicePackMajor)<<std::endl;
-			std::cout<<"wServicePackMinor = "<<COUT_DEC(osvi_ex.wServicePackMinor)<<std::endl;
-			std::cout<<"wSuiteMask = "<<COUT_HEX(osvi_ex.wSuiteMask, 4)<<std::endl;
+			std::cout<<"\twServicePackMajor = "<<COUT_DEC(osvi_ex.wServicePackMajor)<<std::endl;
+			std::cout<<"\twServicePackMinor = "<<COUT_DEC(osvi_ex.wServicePackMinor)<<std::endl;
+			std::cout<<"\twSuiteMask = "<<COUT_HEX(osvi_ex.wSuiteMask, 4)<<std::endl;
 			SuiteMasks.Flags(osvi_ex.wSuiteMask, [](const std::string& label, DWORD value, bool unknown, size_t idx){
 				if (unknown)
-					std::cout<<"\tUNKNOWN FLAG ("<<COUT_HEX(value, 4)<<")"<<std::endl;
+					std::cout<<"\t\tUNKNOWN FLAG ("<<COUT_HEX(value, 4)<<")"<<std::endl;
 				else
-					std::cout<<"\t"<<label<<std::endl;
+					std::cout<<"\t\t"<<label<<std::endl;
 				return false;
 			});
-			std::cout<<"wProductType = "<<COUT_HEX(osvi_ex.wProductType, 2)<<std::endl;
+			std::cout<<"\twProductType = "<<COUT_HEX(osvi_ex.wProductType, 2)<<std::endl;
 			ProductTypes.Enums(osvi_ex.wProductType, [](const std::string& label, DWORD value, bool unknown, size_t idx){
 				if (unknown)
-					std::cout<<"\tUNKNOWN VALUE ("<<COUT_HEX(value, 2)<<")"<<std::endl;
+					std::cout<<"\t\tUNKNOWN VALUE ("<<COUT_HEX(value, 2)<<")"<<std::endl;
 				else
-					std::cout<<"\t"<<label<<std::endl;
+					std::cout<<"\t\t"<<label<<std::endl;
 				return false;
 			});
-			std::cout<<"wReserved = "<<COUT_HEX(osvi_ex.wReserved, 2)<<std::endl;
+			std::cout<<"\twReserved = "<<COUT_HEX(osvi_ex.wReserved, 2)<<std::endl;
 		}
 	} else {
 		std::cout<<"RtlGetVersion and GetVersionEx failed!"<<std::endl;
@@ -147,9 +154,11 @@ int main(int argc, char* argv[])
 	
 	if (fnIsWow64Process) {	
 		//Test if current process is running under WOW64
-		BOOL wow64=FALSE;
-		fnIsWow64Process(GetCurrentProcess(), &wow64);
-		std::cout<<"IsWow64Process() = "<<COUT_BOOL(wow64)<<std::endl;
+		BOOL wow64;
+		if (fnIsWow64Process(GetCurrentProcess(), &wow64))
+			std::cout<<"IsWow64Process = "<<COUT_BOOL(wow64)<<std::endl;
+		else
+			std::cout<<"IsWow64Process failed!"<<std::endl;
 	} else
 		std::cout<<"Can't load IsWow64Process from kernel32.dll!"<<std::endl;
 	
@@ -171,10 +180,9 @@ int main(int argc, char* argv[])
 	
 	std::cout<<std::endl;
 	
-	PrintFileVersion("USER.EXE");
-	PrintFileVersion("KERNEL32.DLL");
-	PrintFileVersion("NTKERN.VXD");
-	PrintFileVersion("DOOM.EXE");
+	PrintFileInformation("KERNEL32.DLL");
+	PrintFileInformation("USER.EXE");
+	PrintFileInformation("NTKERN.VXD");
 	
 	std::cout<<std::endl;
     
@@ -194,7 +202,7 @@ bool GetVersionWrapper(OSVERSIONINFOEX &osvi_ex)
 				osvi_ex.dwBuildNumber=rtl_osvi_ex.dwBuildNumber;
 				osvi_ex.dwPlatformId=rtl_osvi_ex.dwPlatformId;
 				if (rtl_osvi_ex.dwOSVersionInfoSize==sizeof(RTL_OSVERSIONINFOEXW)) {
-					std::cout<<"RTL_OSVERSIONINFOEXW:"<<std::endl;
+					std::cout<<"RtlGetVersion.RTL_OSVERSIONINFOEXW:"<<std::endl;
 					osvi_ex.dwOSVersionInfoSize=sizeof(OSVERSIONINFOEX);
 					osvi_ex.wServicePackMajor=rtl_osvi_ex.wServicePackMajor;
 					osvi_ex.wServicePackMinor=rtl_osvi_ex.wServicePackMinor;
@@ -202,7 +210,7 @@ bool GetVersionWrapper(OSVERSIONINFOEX &osvi_ex)
 					osvi_ex.wProductType=rtl_osvi_ex.wProductType;
 					osvi_ex.wReserved=rtl_osvi_ex.wReserved;
 				} else {
-					std::cout<<"RTL_OSVERSIONINFOW:"<<std::endl;
+					std::cout<<"RtlGetVersion.RTL_OSVERSIONINFOW:"<<std::endl;
 					osvi_ex.dwOSVersionInfoSize=sizeof(OSVERSIONINFO);
 				}
 				return true;
@@ -211,70 +219,41 @@ bool GetVersionWrapper(OSVERSIONINFOEX &osvi_ex)
 	}
 	osvi_ex.dwOSVersionInfoSize=sizeof(OSVERSIONINFOEX);
 	if (GetVersionEx((LPOSVERSIONINFO)&osvi_ex)) {
-		std::cout<<"OSVERSIONINFOEX:"<<std::endl;
+		std::cout<<"GetVersionEx.OSVERSIONINFOEX:"<<std::endl;
 		return true;
 	} else if ((osvi_ex.dwOSVersionInfoSize=sizeof(OSVERSIONINFO), GetVersionEx((LPOSVERSIONINFO)&osvi_ex))) {
-		std::cout<<"OSVERSIONINFO:"<<std::endl;
+		std::cout<<"GetVersionEx.OSVERSIONINFO:"<<std::endl;
 		return true;
 	} else
 		return false;
 }
 
-bool PrintRegistryKey(HKEY hive, const char* keypath, const char* value) {
-	HKEY RegKey;
+bool PrintRegistryKey(HKEY hive, const char* keypath, const char* value) 
+{
+	HKEY reg_key;
 	DWORD buflen;
+	DWORD key_type;
 	bool success=false;
 	
-	char disp_path[strlen(keypath)+strlen(value)+7];
-	switch ((ULONG_PTR)hive) {
-		case (ULONG_PTR)HKEY_CLASSES_ROOT:
-			strcpy(disp_path, "HKCR\\");
-			break;
-		case (ULONG_PTR)HKEY_CURRENT_CONFIG:
-			strcpy(disp_path, "HKCC\\");
-			break;
-		case (ULONG_PTR)HKEY_CURRENT_USER:
-			strcpy(disp_path, "HKCU\\");
-			break;
-		case (ULONG_PTR)HKEY_LOCAL_MACHINE:
-			strcpy(disp_path, "HKLM\\");
-			break;
-		case (ULONG_PTR)HKEY_PERFORMANCE_DATA:
-			strcpy(disp_path, "HKPD\\");
-			break;
-		case (ULONG_PTR)HKEY_PERFORMANCE_NLSTEXT:
-			strcpy(disp_path, "HKPN\\");
-			break;
-		case (ULONG_PTR)HKEY_PERFORMANCE_TEXT:
-			strcpy(disp_path, "HKPT\\");
-			break;
-		case (ULONG_PTR)HKEY_USERS:
-			strcpy(disp_path, "HKU\\");
-			break;
-		default:
-			strcpy(disp_path, "?\\");
-	}
-	
-	strcat(disp_path, keypath);
-	strcat(disp_path, "\\");
-	strcat(disp_path, value);
-	
-	if (RegOpenKeyEx(hive, keypath, 0, KEY_READ, &RegKey)==ERROR_SUCCESS) {
-		if (RegQueryValueEx(RegKey, value, 0, NULL, NULL, &buflen)==ERROR_SUCCESS) {
+	if (RegOpenKeyEx(hive, keypath, 0, KEY_READ, &reg_key)==ERROR_SUCCESS) {
+		if (RegQueryValueEx(reg_key, value, NULL, &key_type, NULL, &buflen)==ERROR_SUCCESS&&key_type==REG_SZ) {
 			char retbuf[buflen];
-			if (RegQueryValueEx(RegKey, value, 0, NULL, (LPBYTE)retbuf, &buflen)==ERROR_SUCCESS) {
-				printf("%s:\n\t\"%s\"\n", disp_path, retbuf);
+			if (RegQueryValueEx(reg_key, value, NULL, &key_type, (LPBYTE)retbuf, &buflen)==ERROR_SUCCESS&&key_type==REG_SZ&&retbuf[buflen-1]=='\0') {
+				std::cout<<RegistryHives(hive)<<"\\"<<keypath<<"\\"<<value<<":\n\t\""<<retbuf<<"\""<<std::endl;
 				success=true;
 			}
 		}
-		RegCloseKey(RegKey);
+		RegCloseKey(reg_key);
 	}
 	
-	if (!success) printf("%s - error while opening!\n", disp_path);
+	if (!success) 
+		std::cout<<RegistryHives(hive)<<"\\"<<keypath<<"\\"<<value<<" - error while opening!"<<std::endl;
+
 	return success;
 }
 
-bool PrintFileVersion(const char* fpath) {
+bool PrintFileInformation(const char* fpath) 
+{
 	//Some Windows system files bear information that can help in detecting Windows version
 	//Most of these files are PE DLLs but some are also LE VXDs
 	//Microsoft vaguely suggests checking VS_FIXEDFILEINFO.FILEVERSION of VERSIONINFO resource and "date" of system files to help detect OS version
@@ -286,70 +265,52 @@ bool PrintFileVersion(const char* fpath) {
 	//"File not found" is a result - absence/presence of specific files also helps in detecting OS version (ex: USB supplement)
 	
 	DWORD buflen;
-	char mddate[20]="?";
-	char crdate[20]="?";
-	
 	if ((buflen=SearchPath(NULL, fpath, NULL, 0, NULL, NULL))) {
 		char fullpth[buflen];
 		if (SearchPath(NULL, fpath, NULL, buflen, fullpth, NULL)) {
+			bool got_info=false;
+			
+			std::cout<<fpath<<":"<<std::endl;
+			
 			HANDLE hFile=CreateFile(fullpth, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 			if (hFile!=INVALID_HANDLE_VALUE) {
-				FILETIME ft;
-				SYSTEMTIME st_utc, st_local;
-				if (GetFileTime(hFile, NULL, NULL, &ft)&&FileTimeToSystemTime(&ft, &st_utc))
-					//FileTime is wrong in HH:MM:SS test should show 12:12:00 - something wrong with timezone
-					sprintf(mddate, "%02d/%02d/%04d %02d:%02d:%02d", st_utc.wDay, st_utc.wMonth, st_utc.wYear, st_utc.wHour, st_utc.wMinute, st_utc.wSecond);
+				FILETIME ft_utc, ft_local;
+				SYSTEMTIME st_local;
 				
-				if (HANDLE hFileMapping=CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL)) {
-					if (LPVOID lpFileBase=MapViewOfFile(hFileMapping, FILE_MAP_READ, 0, 0, 0)) {
-						IMAGE_DOS_HEADER *p_dos_hdr=(IMAGE_DOS_HEADER*)lpFileBase;
-						
-						if (p_dos_hdr->e_magic==IMAGE_DOS_SIGNATURE&&p_dos_hdr->e_lferlc>=0x40) {
-							//IMAGE_DOS_HEADER.e_lferlc is relocation table offset
-							//Offset of 0x40 and more generally indicates that it is offset to "This program cannot be run in DOS mode." stub and executable is actually of "new" type
-							//For "old" type executables IMAGE_DOS_HEADER.e_lfanew member (which is at offset 0x3C) could be well within relocation table itself which results in wild offset to nowhere
-							IMAGE_NT_HEADERS *p_nt_hdr=(IMAGE_NT_HEADERS*)((ULONG_PTR)lpFileBase+p_dos_hdr->e_lfanew);
-							if (p_nt_hdr->Signature==IMAGE_NT_SIGNATURE) {
-								//IMAGE_NT_SIGNATURE is PE signature
-								//Besides PE on Windows it also can be NE (Win 16 executable) and LE (VXD)
-								//NE doesn't contain any version or time information while LE contains Module Version field but on observed ystem VXDs it is always 0
-								//Algorithm to convert UNIX time (time_t) to FILETIME taken from here: https://support.microsoft.com/en-us/kb/167296
-								//Even though FILETIME and LONGLONG returned by Int32x32To64 are both 64-bit numbers, MS suggests no to perform 64-bit arithmetics directly on FILETIME
-								//Instead it suggests using intermediate union LARGE_INTEGER to transfer 64-bit arithmetic result to FILETIME
-								LARGE_INTEGER nix_to_w32;
-								nix_to_w32.QuadPart=Int32x32To64(p_nt_hdr->FileHeader.TimeDateStamp, 10000000)+116444736000000000;
-								ft.dwLowDateTime=nix_to_w32.LowPart;
-								ft.dwHighDateTime=nix_to_w32.HighPart;
-								if (FileTimeToSystemTime(&ft, &st_utc))
-									sprintf(crdate, "%02d/%02d/%04d %02d:%02d:%02d", st_utc.wDay, st_utc.wMonth, st_utc.wYear, st_utc.wHour, st_utc.wMinute, st_utc.wSecond);
-							}
-						}
-						
-						UnmapViewOfFile(lpFileBase);
-					}
-					CloseHandle(hFileMapping);
+				if (GetFileTime(hFile, NULL, NULL, &ft_utc)&&FileTimeToLocalFileTime(&ft_utc, &ft_local)&&FileTimeToSystemTime(&ft_local, &st_local)) {
+					std::cout<<"\tGetFileTime.lpLastWriteTime = "<<COUT_FIX(st_local.wDay, 2)<<"/"<<COUT_FIX(st_local.wMonth, 2)<<"/"<<COUT_FIX(st_local.wYear, 2)<<" "<<COUT_FIX(st_local.wHour, 2)<<":"<<COUT_FIX(st_local.wMinute, 2)<<":"<<COUT_FIX(st_local.wSecond, 2)<<std::endl;
+					got_info=true;
 				}
 				
 				CloseHandle(hFile); 
 			}
 			
-			printf("%s:\n\tGetFileTime.lpLastWriteTime = %s\n\tIMAGE_FILE_HEADER.TimeDateStamp = %s\n", fpath, mddate, crdate);
-			
-			if ((buflen=GetFileVersionInfoSize(fullpth, NULL))) {
+			if ((buflen=GetFileVersionInfoSize(fullpth, NULL))) {	
 				BYTE retbuf[buflen];
 				if (GetFileVersionInfo(fullpth, 0, buflen, (LPVOID)retbuf)) {
-					VS_FIXEDFILEINFO *pfinfo;
-					UINT finfolen;
-					if (VerQueryValue((LPVOID)retbuf, "\\", (LPVOID*)&pfinfo, &finfolen)) {
-						printf("\tVS_FIXEDFILEINFO.FileVersion = %d.%d.%d.%d\n", HIWORD(pfinfo->dwFileVersionMS), LOWORD(pfinfo->dwFileVersionMS), HIWORD(pfinfo->dwFileVersionLS), LOWORD(pfinfo->dwFileVersionLS));
+					LANGANDCODEPAGE *plcp;
+					UINT lcplen;
+					if (VerQueryValue((LPVOID)retbuf, "\\VarFileInfo\\Translation", (LPVOID*)&plcp, &lcplen)&&lcplen>=sizeof(LANGANDCODEPAGE)) {
+						//We are interested only in first ranslation - most system files have only one VERSIONINFO translation
+						char *value;
+						UINT valuelen;
+						std::stringstream qstr;
+						qstr<<std::nouppercase<<std::noshowbase<<std::hex<<std::setfill('0')<<"\\StringFileInfo\\"<<std::setw(4)<<plcp->wLanguage<<std::setw(4)<<plcp->wCodePage<<"\\ProductVersion";
+						if (VerQueryValue((LPVOID)retbuf, qstr.str().c_str(), (LPVOID*)&value, &valuelen)) {
+							std::cout<<"\tVERSIONINFO"<<qstr.str()<<" = \""<<value<<"\""<<std::endl;
+							got_info=true;
+						}
 					}
 				}
 			}
+			
+			if (!got_info)
+				std::cout<<"\tNO INFORMATION FOUND"<<std::endl;
 			
 			return true;
 		}
 	}		
 	
-	printf("%s - error while opening!\n", fpath);
+	std::cout<<fpath<<" - file not found!"<<std::endl;
 	return false;
 }
