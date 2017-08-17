01060f2deba720a24c6fa4e93f8c0dd6d0a4e59d
lcferrum <lcferrum@yandex.com>
2017-08-18 01:27:24 +0400

    More defines

---
+++ extra_defs.h
@@ -0,0 +1,207 @@
+//Defines not found in MinGW's windows.h/winnt.h:
+
+#ifndef PRODUCT_ENTERPRISE_SERVER
+#define PRODUCT_ENTERPRISE_SERVER                   0x0000000A
+#endif
+#ifndef PRODUCT_ENTERPRISE_SERVER_CORE
+#define PRODUCT_ENTERPRISE_SERVER_CORE              0x0000000E
+#endif
+#ifndef PRODUCT_ENTERPRISE_SERVER_IA64
+#define PRODUCT_ENTERPRISE_SERVER_IA64              0x0000000F
+#endif
+#ifndef PRODUCT_ENTERPRISE_SERVER_V
+#define PRODUCT_ENTERPRISE_SERVER_V                 0x00000026
+#endif
+#ifndef PRODUCT_ENTERPRISE_SERVER_CORE_V
+#define PRODUCT_ENTERPRISE_SERVER_CORE_V            0x00000029
+#endif
+#ifndef PRODUCT_PROFESSIONAL_EMBEDDED
+#define PRODUCT_PROFESSIONAL_EMBEDDED               0x0000003A
+#endif
+#ifndef PRODUCT_PRERELEASE
+#define PRODUCT_PRERELEASE                          0x0000004A
+#endif
+#ifndef PRODUCT_PRERELEASE_ARM
+#define PRODUCT_PRERELEASE_ARM                      0x00000051
+#endif
+#ifndef PRODUCT_PRERELEASE_N
+#define PRODUCT_PRERELEASE_N                        0x00000052
+#endif
+#ifndef PRODUCT_PROFESSIONAL_PLUS
+#define PRODUCT_PROFESSIONAL_PLUS                   0x0000005D
+#endif
+#ifndef PRODUCT_EMBEDDED_INDUSTRY_EVAL
+#define PRODUCT_EMBEDDED_INDUSTRY_EVAL              0x00000069
+#endif
+#ifndef PRODUCT_EMBEDDED_INDUSTRY_E_EVAL
+#define PRODUCT_EMBEDDED_INDUSTRY_E_EVAL            0x0000006A
+#endif
+#ifndef PRODUCT_EMBEDDED_EVAL
+#define PRODUCT_EMBEDDED_EVAL                       0x0000006B
+#endif
+#ifndef PRODUCT_EMBEDDED_E_EVAL
+#define PRODUCT_EMBEDDED_E_EVAL                     0x0000006C
+#endif
+#ifndef PRODUCT_NANO_SERVER
+#define PRODUCT_NANO_SERVER                         0x0000006D
+#endif
+#ifndef PRODUCT_CLOUD_STORAGE_SERVER
+#define PRODUCT_CLOUD_STORAGE_SERVER                0x0000006E
+#endif
+#ifndef PRODUCT_CORE_CONNECTED
+#define PRODUCT_CORE_CONNECTED                      0x0000006F
+#endif
+#ifndef PRODUCT_PROFESSIONAL_STUDENT
+#define PRODUCT_PROFESSIONAL_STUDENT                0x00000070
+#endif
+#ifndef PRODUCT_CORE_CONNECTED_N
+#define PRODUCT_CORE_CONNECTED_N                    0x00000071
+#endif
+#ifndef PRODUCT_PROFESSIONAL_STUDENT_N
+#define PRODUCT_PROFESSIONAL_STUDENT_N              0x00000072
+#endif
+#ifndef PRODUCT_CORE_CONNECTED_SINGLELANGUAGE
+#define PRODUCT_CORE_CONNECTED_SINGLELANGUAGE       0x00000073
+#endif
+#ifndef PRODUCT_CORE_CONNECTED_COUNTRYSPECIFIC
+#define PRODUCT_CORE_CONNECTED_COUNTRYSPECIFIC      0x00000074
+#endif
+#ifndef PRODUCT_CONNECTED_CAR
+#define PRODUCT_CONNECTED_CAR                       0x00000075
+#endif
+#ifndef PRODUCT_INDUSTRY_HANDHELD
+#define PRODUCT_INDUSTRY_HANDHELD                   0x00000076
+#endif
+#ifndef PRODUCT_PPI_PRO
+#define PRODUCT_PPI_PRO                             0x00000077
+#endif
+#ifndef PRODUCT_ARM64_SERVER
+#define PRODUCT_ARM64_SERVER                        0x00000078
+#endif
+#ifndef PRODUCT_EDUCATION
+#define PRODUCT_EDUCATION                           0x00000079
+#endif
+#ifndef PRODUCT_EDUCATION_N
+#define PRODUCT_EDUCATION_N                         0x0000007A
+#endif
+#ifndef PRODUCT_IOTUAP
+#define PRODUCT_IOTUAP                              0x0000007B
+#endif
+#ifndef PRODUCT_CLOUD_HOST_INFRASTRUCTURE_SERVER
+#define PRODUCT_CLOUD_HOST_INFRASTRUCTURE_SERVER    0x0000007C
+#endif
+#ifndef PRODUCT_ENTERPRISE_S
+#define PRODUCT_ENTERPRISE_S                        0x0000007D
+#endif
+#ifndef PRODUCT_ENTERPRISE_S_N
+#define PRODUCT_ENTERPRISE_S_N                      0x0000007E
+#endif
+#ifndef PRODUCT_PROFESSIONAL_S
+#define PRODUCT_PROFESSIONAL_S                      0x0000007F
+#endif
+#ifndef PRODUCT_PROFESSIONAL_S_N
+#define PRODUCT_PROFESSIONAL_S_N                    0x00000080
+#endif
+#ifndef PRODUCT_ENTERPRISE_S_EVALUATION
+#define PRODUCT_ENTERPRISE_S_EVALUATION             0x00000081
+#endif
+#ifndef PRODUCT_ENTERPRISE_S_N_EVALUATION
+#define PRODUCT_ENTERPRISE_S_N_EVALUATION           0x00000082
+#endif
+#ifndef PRODUCT_IOTUAPCOMMERCIAL
+#define PRODUCT_IOTUAPCOMMERCIAL                    0x00000083
+#endif
+#ifndef PRODUCT_MOBILE_ENTERPRISE
+#define PRODUCT_MOBILE_ENTERPRISE                   0x00000085
+#endif
+#ifndef PRODUCT_HOLOGRAPHIC
+#define PRODUCT_HOLOGRAPHIC                         0x00000087
+#endif
+#ifndef PRODUCT_PRO_SINGLE_LANGUAGE
+#define PRODUCT_PRO_SINGLE_LANGUAGE                 0x0000008A
+#endif
+#ifndef PRODUCT_PRO_CHINA
+#define PRODUCT_PRO_CHINA                           0x0000008B
+#endif
+#ifndef PRODUCT_ENTERPRISE_SUBSCRIPTION
+#define PRODUCT_ENTERPRISE_SUBSCRIPTION             0x0000008C
+#endif
+#ifndef PRODUCT_ENTERPRISE_SUBSCRIPTION_N
+#define PRODUCT_ENTERPRISE_SUBSCRIPTION_N           0x0000008D
+#endif
+#ifndef PRODUCT_DATACENTER_NANO_SERVER
+#define PRODUCT_DATACENTER_NANO_SERVER              0x0000008F
+#endif
+#ifndef PRODUCT_STANDARD_NANO_SERVER
+#define PRODUCT_STANDARD_NANO_SERVER                0x00000090
+#endif
+#ifndef PRODUCT_DATACENTER_A_SERVER_CORE
+#define PRODUCT_DATACENTER_A_SERVER_CORE            0x00000091
+#endif
+#ifndef PRODUCT_STANDARD_A_SERVER_CORE
+#define PRODUCT_STANDARD_A_SERVER_CORE              0x00000092
+#endif
+#ifndef PRODUCT_DATACENTER_WS_SERVER_CORE
+#define PRODUCT_DATACENTER_WS_SERVER_CORE           0x00000093
+#endif
+#ifndef PRODUCT_STANDARD_WS_SERVER_CORE
+#define PRODUCT_STANDARD_WS_SERVER_CORE             0x00000094
+#endif
+#ifndef PRODUCT_UTILITY_VM
+#define PRODUCT_UTILITY_VM                          0x00000095
+#endif
+#ifndef PRODUCT_DATACENTER_EVALUATION_SERVER_CORE
+#define PRODUCT_DATACENTER_EVALUATION_SERVER_CORE   0x0000009F
+#endif
+#ifndef PRODUCT_STANDARD_EVALUATION_SERVER_CORE
+#define PRODUCT_STANDARD_EVALUATION_SERVER_CORE     0x000000A0
+#endif
+#ifndef PRODUCT_PRO_WORKSTATION
+#define PRODUCT_PRO_WORKSTATION                     0x000000A1
+#endif
+#ifndef PRODUCT_PRO_WORKSTATION_N
+#define PRODUCT_PRO_WORKSTATION_N                   0x000000A2
+#endif
+#ifndef PRODUCT_PRO_FOR_EDUCATION
+#define PRODUCT_PRO_FOR_EDUCATION                   0x000000A4
+#endif
+#ifndef PRODUCT_PRO_FOR_EDUCATION_N
+#define PRODUCT_PRO_FOR_EDUCATION_N                 0x000000A5
+#endif
+#ifndef PRODUCT_AZURE_SERVER_CORE
+#define PRODUCT_AZURE_SERVER_CORE                   0x000000A8
+#endif
+#ifndef PRODUCT_AZURE_NANO_SERVER
+#define PRODUCT_AZURE_NANO_SERVER                   0x000000A9
+#endif
+#ifndef PRODUCT_ENTERPRISEG
+#define PRODUCT_ENTERPRISEG                         0x000000AB
+#endif
+#ifndef PRODUCT_ENTERPRISEGN
+#define PRODUCT_ENTERPRISEGN                        0x000000AC
+#endif
+#ifndef PRODUCT_CLOUD
+#define PRODUCT_CLOUD                               0x000000B2
+#endif
+#ifndef PRODUCT_CLOUDN
+#define PRODUCT_CLOUDN                              0x000000B3
+#endif
+#ifndef SM_WEPOS
+#define SM_WEPOS                                    0x2003
+#endif
+#ifndef SM_FUNDAMENTALS
+#define SM_FUNDAMENTALS                             0x2004
+#endif
+#ifndef VER_PLATFORM_WIN32_CE
+#define VER_PLATFORM_WIN32_CE                       3
+#endif
+#ifndef FILE_VER_GET_NEUTRAL
+#define FILE_VER_GET_NEUTRAL                        0x002
+#endif
+
+#define XP_CONTEXT_GUID     {0xbeb1b341, 0x6837, 0x4c83, {0x83, 0x66, 0x2b, 0x45, 0x1e, 0x7c, 0xe6, 0x9b}}
+#define VISTA_CONTEXT_GUID  {0xe2011457, 0x1546, 0x43c5, {0xa5, 0xfe, 0x00, 0x8d, 0xee, 0xe3, 0xd3, 0xf0}}
+#define WIN7_CONTEXT_GUID   {0x35138b9a, 0x5d96, 0x4fbd, {0x8e, 0x2d, 0xa2, 0x44, 0x02, 0x25, 0xf9, 0x3a}}
+#define WIN8_CONTEXT_GUID   {0x4a2f28e3, 0x53b9, 0x4441, {0xba, 0x9c, 0xd6, 0x9d, 0x4a, 0x4a, 0x6e, 0x38}}
+#define WIN81_CONTEXT_GUID  {0x1f676c76, 0x80e1, 0x4239, {0x95, 0xbb, 0x83, 0xd0, 0xf6, 0xd0, 0xda, 0x78}}
+#define WIN10_CONTEXT_GUID  {0x8e0f7a12, 0xbfb3, 0x4fe8, {0xb9, 0xa5, 0x48, 0xfd, 0x50, 0xa1, 0x5a, 0x9a}}
